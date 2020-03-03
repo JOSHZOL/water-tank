9f748b24753a79a6da2df422b6283604809911a6
Josh Riddlestone <jo5hman@email.com>
2020-03-03 19:34:59 +1300

    fixed duration undefined and time update bug

--- main.js
+++ main.js
@@ -2,21 +2,24 @@ let results;
 
 (function getData() {
     $.getJSON('https://api.thingspeak.com/channels/950519/feeds.json?api_key=NHWOOIO02CQMDZ25&results=1', gotResults);
-    setTimeout(getData, 15000);
+    setTimeout(getData, 5000);
 })();
 
 function gotResults(data) {
     results = data;
-    setTime();
     document.getElementById("depth-text").innerHTML = `${results.feeds[0].field3}L`;
     document.getElementById("water").style.height = `${results.feeds[0].field2}%`;
 }
 
-function setTime()
+(function setTime()
 {
-    document.getElementById("time-text").innerHTML = timeSince(results.feeds[0].created_at);
+    if (typeof(results) != 'undefined') 
+    {
+        document.getElementById("time-text").innerHTML = timeSince(results.feeds[0].created_at);
+    }
+    
     setTimeout(setTime, 1000);
-}
+})();
 
 let DURATION_IN_SECONDS = {
     epochs: ['year', 'month', 'day', 'hour', 'minute', 'second'],
@@ -47,13 +50,13 @@ function getDuration(seconds) {
 function timeSince(date) {
     var seconds = Math.floor((new Date() - new Date(date)) / 1000);
     var duration = getDuration(seconds);
-    if (duration.interval != undefined)
+    if (typeof(duration) != 'undefined')
     {
         var suffix = (duration.interval > 1 || duration.interval === 0) ? 's' : '';
         return duration.interval + ' ' + duration.epoch + suffix + ' ago';
     }
     else
     {
-        return 'Moments ago';
+        return 'updating...';
     }
 };
