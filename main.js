07356f1d3b47fb5e8abadf7a5fc00947d0a37cfb
Josh Riddlestone <jo5hman@email.com>
2020-03-03 19:08:38 +1300

    Added auto update for data

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
-$.getJSON('https://api.thingspeak.com/channels/950519/feeds.json?api_key=NHWOOIO02CQMDZ25&results=1', gotResults);
+let results;
 
-function gotResults(results) {
-    document.getElementById("time-text").innerHTML = `${timeSince(results.feeds[0].created_at)} ago`;
+(function getData() {
+    $.getJSON('https://api.thingspeak.com/channels/950519/feeds.json?api_key=NHWOOIO02CQMDZ25&results=1', gotResults);
+    setTimeout(getData, 15000);
+})();
+
+function gotResults(data) {
+    results = data;
+    setTime();
     document.getElementById("depth-text").innerHTML = `${results.feeds[0].field3}L`;
     document.getElementById("water").style.height = `${results.feeds[0].field2}%`;
 }
 
+function setTime()
+{
+    document.getElementById("time-text").innerHTML = timeSince(results.feeds[0].created_at);
+    setTimeout(setTime, 1000);
+}
+
 let DURATION_IN_SECONDS = {
-    epochs: ['year', 'month', 'day', 'hour', 'minute'],
+    epochs: ['year', 'month', 'day', 'hour', 'minute', 'second'],
     year: 31536000,
     month: 2592000,
     day: 86400,
     hour: 3600,
-    minute: 60
+    minute: 60,
+    second: 1
 };
 
 function getDuration(seconds) {
@@ -34,6 +47,13 @@ function getDuration(seconds) {
 function timeSince(date) {
     var seconds = Math.floor((new Date() - new Date(date)) / 1000);
     var duration = getDuration(seconds);
-    var suffix = (duration.interval > 1 || duration.interval === 0) ? 's' : '';
-    return duration.interval + ' ' + duration.epoch + suffix;
+    if (duration.interval != undefined)
+    {
+        var suffix = (duration.interval > 1 || duration.interval === 0) ? 's' : '';
+        return duration.interval + ' ' + duration.epoch + suffix + ' ago';
+    }
+    else
+    {
+        return 'Moments ago';
+    }
 };
