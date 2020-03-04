efb324591a852fcd3d8a35ab4beb1bf5c44dce42
Josh Riddlestone <jo5hman@email.com>
2020-03-04 19:13:16 +1300

    gets median from last 10 depths and calculates values from depth
    gets median from last 10 depths and calculates water percentage and litres from median water depth only

--- main.js
+++ main.js
@@ -1,39 +1,65 @@
 let results;
+let waterDepth = 0;
+let waterPercentage = 0;
+let waterLitres = 0;
+let numberOfTanks = 2;
 
 (function getData() {
-    $.getJSON('https://api.thingspeak.com/channels/950519/feeds.json?api_key=NHWOOIO02CQMDZ25&results=1', gotResults);
-    setTimeout(getData, 5000);
+    $.getJSON('https://api.thingspeak.com/channels/950519/fields/1.json?api_key=NHWOOIO02CQMDZ25&results=10', gotResults);
+    setTimeout(getData, 30000);
 })();
 
 function gotResults(data) {
     results = data;
-    document.getElementById("depth-text").innerHTML = `${results.feeds[0].field3}L`;
-    document.getElementById("water").style.height = `${results.feeds[0].field2}%`;
+    waterDepthValues();
+    calculateValues();
+    updateHtml();
+}
+
+function waterDepthValues() {
+    let depths = [];
+
+    for (let i = 0; i < results.feeds.length; i++) {
+        depths.push(results.feeds[i].field1);
+    }
+
+    waterDepth = median(depths);
+}
+
+function calculateValues() {
+    waterPercentage = (waterDepth / 218);
+    waterLitres = ((Math.PI * (1.8 * 1.8) * 2.18) * waterPercentage) * 10;
+    waterLitres = Math.floor(waterLitres);
+    waterLitres *= 100;
+    waterLitres *= numberOfTanks;
+    waterPercentage *= 100;
+}
+
+function updateHtml() {
+    document.getElementById("depth-text").innerHTML = `${waterLitres}L`;
+    document.getElementById("water").style.height = `${waterPercentage}%`;
 }
 
 (function setTime() {
-    if (typeof(results) != 'undefined') 
-    {
-        document.getElementById("time-text").innerHTML = timeSince(results.feeds[0].created_at);
+    if (typeof(results) != 'undefined') {
+        document.getElementById("time-text").innerHTML = timeSince(results.feeds[results.feeds.length - 1].created_at);
     }
     
     setTimeout(setTime, 1000);
 })();
 
 let DURATION_IN_SECONDS = {
-    epochs: ['year', 'month', 'day', 'hour', 'minute', 'second'],
-    year: 31536000,
-    month: 2592000,
+    epochs: ['day', 'hour', 'minute'],
     day: 86400,
     hour: 3600,
-    minute: 60,
-    second: 1
+    minute: 60
 };
 
 function getDuration(seconds) {
-    var epoch, interval;
+    let epoch;
+    let interval;
 
-    for (var i = 0; i < DURATION_IN_SECONDS.epochs.length; i++) {
+    for (let i = 0; i < DURATION_IN_SECONDS.epochs.length; i++) {
         epoch = DURATION_IN_SECONDS.epochs[i];
         interval = Math.floor(seconds / DURATION_IN_SECONDS[epoch]);
         if (interval >= 1) {
@@ -43,20 +69,32 @@ function getDuration(seconds) {
             };
         }
     }
-
 };
 
 function timeSince(date) {
-    var seconds = Math.floor((new Date() - new Date(date)) / 1000);
-    var duration = getDuration(seconds);
+    let seconds = Math.floor((new Date() - new Date(date)) / 1000);
+    let duration = getDuration(seconds);
 
-    if (typeof(duration) != 'undefined')
-    {
-        var suffix = (duration.interval > 1 || duration.interval === 0) ? 's' : '';
+    if (typeof(duration) != 'undefined') {
+        let suffix = (duration.interval > 1 || duration.interval === 0) ? 's' : '';
         return duration.interval + ' ' + duration.epoch + suffix + ' ago';
-    }
-    else
-    {
-        return 'moments ago';
+    } else {
+        return 'A moment ago';
     }
 };
+
+function median(numbers) {
+    let median = 0; 
+    let numsLen = numbers.length;
+    numbers.sort();
+ 
+    if (numsLen % 2 === 0) { // is even
+        // average of two middle numbers
+        median = (parseInt(numbers[(numsLen / 2) - 1]) + parseInt(numbers[(numsLen / 2)])) / 2;
+    } else { // is odd
+        // middle number only
+        median = numbers[(numsLen - 1) / 2];
+    }
+ 
+    return median;
+}
